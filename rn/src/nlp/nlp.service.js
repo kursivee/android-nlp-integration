47c8cf691e3c5184208745b9a42968b77fe5f10e
kursivee <kursivee.dev@gmail.com>
2019-07-23 23:34:17 -0400

    Call process to preload nlp

--- rn/src/nlp/nlp.service.js
+++ rn/src/nlp/nlp.service.js
@@ -1,5 +1,8 @@
 import { RnNlpManager } from 'node-nlp'
 
 const manager = new RnNlpManager({ languages: ['en'] })
+// Need to find out why this is necessary.
+// Not calling .process initially leads to load time on first call
+manager.process('en','')
 
 module.exports = manager
